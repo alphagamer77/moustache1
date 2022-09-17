048d8c200cdfeff40bdd56a73632f497192dd29a
alphagamer77 <90268397+alphagamer77@users.noreply.github.com>
2022-09-17 16:51:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setUp(){
+canvas=createCanvas(480,480);
+canvas.center();   
+}
+
+function draw(){
+}
+
+function takeSnapshot(){
+save('myFilteredImage.png');    
+}
